d2a5c5df6479b5a5c46d638579d209a0b0737b8c
Nick Fry <nicholas.a.fry@gmail.com>
2021-10-13 07:11:14 -0500

    security update

--- doe2022geothermalcc_project/settings.py
+++ doe2022geothermalcc_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h4t26#&!^64bixxa7ie7e+zwrdsqp!(__ejjt_%b2*#0*nmsxc'
+SECRET_KEY = os.environ[‘SECRET_KEY’]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
