a05d76db86c565035fa449226d0b84fc30a8c067
Nicholas Fry <60554125+NicholasFry@users.noreply.github.com>
2021-11-24 16:05:28 -0600

    update to framework

--- doe2022geothermalcc_project/settings.py
+++ doe2022geothermalcc_project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ["SECRET_KEY"] #When deployed
 # SECRET_KEY = "" #when local
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'doe2022geothermalcc_project.urls'
@@ -125,6 +126,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
