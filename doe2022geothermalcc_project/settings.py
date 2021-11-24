0bd1dc5b2efae6523b0d96a9ed410a30c3691011
Nicholas Fry <60554125+NicholasFry@users.noreply.github.com>
2021-11-24 15:56:40 -0600

    update to the color scheme, some responsive design

--- doe2022geothermalcc_project/settings.py
+++ doe2022geothermalcc_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+from decouple import config 
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["SECRET_KEY"]
+SECRET_KEY = os.environ["SECRET_KEY"] #When deployed
+# SECRET_KEY = "" #when local
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
